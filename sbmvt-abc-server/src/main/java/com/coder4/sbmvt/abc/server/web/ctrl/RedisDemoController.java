4ba2518aecde4dcf9a517994e7b18f4592643c78
coder4 <lihy@fenbi.com>
2017-12-11 18:32:46 +0800

    MOD: Redis controller
    Change-Id: I4ad11f1c76e5bcbe92f284c3c9aa8c29a43e716c

--- sbmvt-abc-server/src/main/java/com/coder4/sbmvt/abc/server/web/ctrl/RedisDemoController.java
+++ sbmvt-abc-server/src/main/java/com/coder4/sbmvt/abc/server/web/ctrl/RedisDemoController.java
@@ -7,7 +7,9 @@
 package com.coder4.sbmvt.abc.server.web.ctrl;
 
 import com.coder4.sbmvt.abc.constant.AbcConstant;
+import com.coder4.sbmvt.abc.server.data.Abc;
 import com.coder4.sbmvt.abc.server.storage.spi.AbcStorage2;
+import org.apache.commons.lang.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,16 +32,16 @@ public class RedisDemoController {
     )
     public String getLessonUserReport(
             @PathVariable(value = "id") String id) {
-        //Abc abc = new Abc();
-        //abc.setId(1);
-        //abc.setSbc(RandomStringUtils.randomAlphanumeric(6));
-        //abcStorage2.saveAbc(id, abc);
+        Abc abc = new Abc();
+        abc.setId(1);
+        abc.setSbc(RandomStringUtils.randomAlphanumeric(6));
+        abcStorage2.saveAbc(id, abc);
         //System.out.println(abcStorage2.getAbc("ddd").orElse(null));
 
-        abcStorage2.getString("dd");
+        //abcStorage2.getString("1");
 
         //abcStorage2.saveString(id, RandomStringUtils.randomAlphanumeric(10));
-        return "hello, " + id + "," + abcStorage2.getString(id);
+        return "hello, " + id + "," + abcStorage2.getAbc(id);
 
 
         /*
